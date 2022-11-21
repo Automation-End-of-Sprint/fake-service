004fbb6c4f48f5bb5bb03d7a221510c21b045e63
Patrick MacbookPro <p.oconnormsu@gmail.com>
2022-11-20 23:09:59 -0700

    Update for github actions path

--- ProdValidationUtil/ProdValidationUtil.py
+++ ProdValidationUtil/ProdValidationUtil.py
@@ -119,13 +119,13 @@ def createFilePath(serviceList):
             pass  # TODO Might want to reorganize this but for now it works
         elif service.environmentType == Constants.ValidationEnvConstants.TRAIN:
             filePath = Path(
-                "/develop/" + service.serviceName + "/" + service.serviceName + "-" + service.environmentType.value + ".properties")
+                "./develop/" + service.serviceName + "/" + service.serviceName + "-" + service.environmentType.value + ".properties")
             service.setFilePath(filePath)
         elif service.environmentType == Constants.ValidationEnvConstants.DEVELOPBaseProperties:
-            filePath = Path("/develop/" + service.serviceName + "/" + service.serviceName + ".properties")
+            filePath = Path("./develop/" + service.serviceName + "/" + service.serviceName + ".properties")
             service.setFilePath(filePath)
         elif service.environmentType == Constants.ValidationEnvConstants.MASTERBaseProperties:
-            filePath = Path("/master/" + service.serviceName + "/" + service.serviceName + ".properties")
+            filePath = Path("./master/" + service.serviceName + "/" + service.serviceName + ".properties")
             service.setFilePath(filePath)
         elif service.environmentType == Constants.ValidationEnvConstants.PROD or service.environmentType == Constants.ValidationEnvConstants.PRODAP:
             filePath = Path(
@@ -133,7 +133,7 @@ def createFilePath(serviceList):
             service.setFilePath(filePath)
         else:
             filePath = Path(
-                "/develop/" + service.serviceName + "/" + service.serviceName + "-" + service.environmentType.value + ".properties")
+                "./develop/" + service.serviceName + "/" + service.serviceName + "-" + service.environmentType.value + ".properties")
             bashScript = "ls"
             os.system(bashScript)
             service.setFilePath(filePath)
